0986190a45b27fa7e08b2814c2c0c6961cb316ec
Santosh Marigowda <santosh.marigowda@abcam.com>
2021-11-13 19:17:29 +0000

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
+import SmallCard from "../components/SmallCard";
+import { projectIcons } from "../components/Icons";
 
-import { projects } from '../utils/projectsData';
+import { projects } from "../utils/projectsData";
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
+    <h1 className="header">How are JIRA tickets progressing...?</h1>
+    {/* <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
         return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
       })}
-    </div>
+    </div> */}
   </div>
 );
 
