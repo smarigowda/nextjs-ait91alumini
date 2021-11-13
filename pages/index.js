621f9f19d4a2d411bd1312670775af9f6e5b81b0
Santosh Marigowda <santosh.marigowda@abcam.com>
2021-11-13 19:38:02 +0000

    using d3

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,12 @@ import { projectIcons } from "../components/Icons";
 
 import { projects } from "../utils/projectsData";
 
+import { MyApp } from "../components/myD3Component";
+
 const Home = () => (
   <div className="home">
     <h1 className="header">How are JIRA tickets progressing...?</h1>
+    <MyApp></MyApp>
     {/* <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
