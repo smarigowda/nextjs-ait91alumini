fd99938b40987847fbdb151c86d66886b6ca3795
Santosh Marigowda <santosh.marigowda@abcam.com>
2021-11-14 18:52:43 +0000

    added list of people

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ const Home = () => (
   <div className="home">
     <h1 className="header">AIT Alumini 91</h1>
     <img src="/images/ait-alumini-meet.jpeg"></img>
+    <h1>List of people confirmed to be there in the meet...</h1>
+    <h2 className="confirmed-name">Dattatreya</h2>
+    <h2 className="confirmed-name">Somashekar</h2>
+    <h2 className="confirmed-name">Ramachanndra pai</h2>
+    <h2 className="confirmed-name">Chandra</h2>
+    <h2 className="confirmed-name">Rajesh Roy</h2>
+    <h2 className="confirmed-name">Narasimha Reddy</h2>
+    <h2 className="confirmed-name">Gururaj</h2>
+    <h2 className="confirmed-name">Anand Kumar H L</h2>
+    <h2 className="confirmed-name">Balaraj</h2>
   </div>
 );
 
